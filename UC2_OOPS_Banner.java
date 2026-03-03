74656e196db15c37dd858b9a4b9486c91f6a7210
S-lakshay-12 <lakshay.lsk12@gmail.com>
2026-03-03 22:18:10 +0530

    Add files via upload

---
+++ UC2_OOPS_Banner.java
@@ -0,0 +1,16 @@
+// UC2 - Display "OOPS" as ASCII Banner using manual print statements
+
+public class UC2_OOPS_Banner {
+
+    public static void main(String[] args) {
+
+        System.out.println(" *****     *****    ******     *****  ");
+        System.out.println("*     *   *     *   *     *   *     * ");
+        System.out.println("*     *   *     *   *     *   *       ");
+        System.out.println("*     *   *     *   ******     *****  ");
+        System.out.println("*     *   *     *   *              *  ");
+        System.out.println("*     *   *     *   *        *     *  ");
+        System.out.println(" *****     *****    *         *****   ");
+
+    }
+}
